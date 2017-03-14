74c3397fa251fbeb750b9c6e6f130127544a8761
Z.J. van de Weg <git@zjvandeweg.nl>
2017-03-14 08:05:58 +0100

    Improve fail message for test

--- verify_templates.rb
+++ verify_templates.rb
@@ -6,7 +6,13 @@ uri = URI.parse 'https://gitlab.com/api/v3/ci/lint'
 Dir.glob("#{File.dirname(__FILE__)}/**/*.yml").each do |file|
   response = Net::HTTP.post_form(uri, content: File.read(file))
 
-  file = file.match(/((\w|\+|#)+)\.gitlab-ci/)[1]
+  match = file.match(/(?<file>(\w|\+|#)+)\.gitlab-ci.yml/)
+  template_name = match ? match[:file] : nil
+
+  unless template_name
+    puts "Invalid file format for #{file}"
+    exit(1)
+  end
 
   if JSON.parse(response.body)['status'] == 'valid'
     puts "\e[32mvalid\e[0m: #{file}" # Color 'valid' green
@@ -17,5 +23,6 @@ Dir.glob("#{File.dirname(__FILE__)}/**/*.yml").each do |file|
 end
 
 # Given we test all the templates, the coverage is 100%, always. To showcase
-# how this is done, we print it here.
+# how this is done, we print it here. The regex to parse this would be:
+# /Coverage:\s(d+)%/
 puts "Coverage: 100%"
